5ce56b84ac630ec1e6b888be69e4e93b0cb7f224
taufiksty <taufik.satya09@gmail.com>
2023-02-16 20:37:37 +0700

    still figuring why connect mongodb very messy

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -1,16 +1,28 @@
-const express = require("express");
-const tasksRoutes = require("./routes/tasks-route");
-const morgan = require("morgan");
-// const path = require("path");
+// const express = require("express");
+const connectDB = require("./db/connect");
+// const tasksRoutes = require("./routes/tasks-route");
+// const morgan = require("morgan");
+require("dotenv").config();
+// // const path = require("path");
 
-const app = express();
+// const app = express();
 
-// app.use(express.static(path.join(__dirname, "./public")));
-app.use(morgan("tiny"));
-app.use(express.json());
-app.use("/api/v1/tasks", tasksRoutes);
+// // app.use(express.static(path.join(__dirname, "./public")));
+// app.use(morgan("tiny"));
+// app.use(express.json());
+// app.use("/api/v1/tasks", tasksRoutes);
 
-const port = 3000;
-app.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
-});
+// const PORT = 3000;
+
+// const connectToDB = async () => {
+// 	try {
+// 		await connectDB(process.env.MONGO_URI);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
+
+connectDB(process.env.MONGO_URI);
+// app.listen(PORT, () => {
+// 	console.log(`Server is listening on port ${PORT}`);
+// });
